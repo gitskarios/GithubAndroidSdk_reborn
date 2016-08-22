f174329a0a8b196cc51a010749ac04e62e6d8b5f
Bernat Borrás Paronella <bernatbor15@gmail.com>
2016-08-22 19:42:35 +0200

    Update org events

--- src/main/java/com/alorma/github/sdk/core/User.java
+++ src/main/java/com/alorma/github/sdk/core/User.java
@@ -1,10 +1,14 @@
 package com.alorma.github.sdk.core;
 
+import com.google.gson.annotations.SerializedName;
+
 public class User {
 
   private int id;
   private String login;
   private String name;
+  @SerializedName("avatar_url")
+  private String avatar;
 
   public User() {
   }
@@ -31,5 +35,9 @@ public class User {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public String getAvatar() {
+    return avatar;
   }
 }
