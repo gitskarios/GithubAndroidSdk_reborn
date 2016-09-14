49ab19b8f77f2cbc4b96790d43cac96998aed592
Bernat Borrás Paronella <bernatbor15@gmail.com>
2016-09-14 21:04:40 +0200

    Add new Issue search request

--- src/main/java/com/alorma/github/sdk/core/issues/CloudUsersIssuesDataSource.java
+++ src/main/java/com/alorma/github/sdk/core/issues/CloudUsersIssuesDataSource.java
@@ -4,6 +4,7 @@ import com.alorma.github.sdk.core.User;
 import com.alorma.github.sdk.core.datasource.CloudDataSource;
 import com.alorma.github.sdk.core.datasource.RestWrapper;
 import com.alorma.github.sdk.core.datasource.SdkItem;
+import com.alorma.github.sdk.core.issue.IssuesSearchRequest;
 import com.alorma.github.sdk.core.issue.IssuesSearchService;
 import com.alorma.github.sdk.core.repositories.Repo;
 import java.io.IOException;
@@ -11,30 +12,22 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Response;
 import rx.Observable;
-import rx.functions.Func0;
-import rx.functions.Func1;
 
-public class CloudUsersIssuesDataSource extends CloudDataSource<Void, List<Issue>> {
+public class CloudUsersIssuesDataSource extends CloudDataSource<IssuesSearchRequest, List<Issue>> {
 
-  private final String action;
-  private final String name;
-
-  public CloudUsersIssuesDataSource(RestWrapper restWrapper, String action, String name) {
+  public CloudUsersIssuesDataSource(RestWrapper restWrapper) {
     super(restWrapper);
-    this.action = action;
-    this.name = name;
   }
 
   @Override
-  protected Observable<SdkItem<List<Issue>>> execute(final SdkItem<Void> data, final RestWrapper service) {
+  protected Observable<SdkItem<List<Issue>>> execute(final SdkItem<IssuesSearchRequest> data, final RestWrapper service) {
     return Observable.defer(() -> {
-      String query = String.format(IssuesSearchService.QUERY, "open", action, name);
       IssuesSearchService issuesService = service.get();
       Call<IssueSearchResponse> call;
       if (data.getPage() != null) {
-        call = issuesService.userIssues(query, data.getPage());
+        call = issuesService.userIssues(data.getK().build(), data.getPage());
       } else {
-        call = issuesService.userIssues(query);
+        call = issuesService.userIssues(data.getK().build());
       }
 
       try {
@@ -62,7 +55,7 @@ public class CloudUsersIssuesDataSource extends CloudDataSource<Void, List<Issue
             url = url.replaceAll("https://", "");
             url = url.replaceAll("http://", "");
             url = url.replace("api.github.com/repos/", "");
-            String[]parts = url.split("/");
+            String[] parts = url.split("/");
             String owner = parts[0];
             String repo = parts[1];
             Repo repository = new Repo();
